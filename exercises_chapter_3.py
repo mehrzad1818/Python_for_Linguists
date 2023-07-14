41f0d6ac7ff3365e5c0c49f720fc57037cdb32d0
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-07-15 01:56:17 +0330

    Create exercises_chapter_3.py

---
+++ exercises_chapter_3.py
@@ -0,0 +1,29 @@
+# Control Structures
+
+# if is the first control structure we're going to learn,
+# it's basically used for conditional application,
+# where a given test clause is followed by one or several block.
+# based on the test clause's answer, the conditions are met.
+
+# %%
+# control1.py
+
+if 2 + 2 == 4:
+    print(f"The answer is {4}.")
+
+# %%
+# control2.py
+
+# The code below produces error, since it's not indented.
+
+if 3 - 1 > -3:
+print("You are right.")
+
+# %%
+# control3.py
+
+if isinstance(23, int):
+    print("This is a number.")
+    print("I'm sure it's a number.")
+    print("Come on, dude!")
+# %%
